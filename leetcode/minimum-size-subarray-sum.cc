2e7e14f83daf7d45777e9782b41a80a969c392e1
xunzhang <xunzhangthu@gmail.com>
2015-10-18 18:31:39 +0800

    update

--- leetcode/minimum-size-subarray-sum.cc
+++ leetcode/minimum-size-subarray-sum.cc
@@ -9,31 +9,39 @@ class Solution {
       sz += 1;
       while(sum >= s) {
         result = min(result, sz);
-          sum -= nums[start];
-          start ++;
-          sz -= 1;
-        }
+        sum -= nums[start];
+        start ++;
+        sz -= 1;
+      }
     }
     return result == INT_MAX ? 0 : result;
   }
 
   // O(nlogn) solution
-  int minSubArrayLen2(int s, vector<int>& nums) {
-    // TODO
+  int minSubArrayLen(int s, vector<int>& nums) {
+    int sz = INT_MAX;
+    if(nums.size() == 0) return 0;
+    vector<int> sums(nums.size() + 1, 0);
+    for (int i = 1; i <= nums.size(); i++) sums[i] = sums[i-1] + nums[i-1];
+    for(int i = 0; i < sums.size(); ++i) {
+      int j = binarySearch(sums, sums[i] + s, i + 1, sums.size() - 1);
+      if(j == sums.size()) break;
+      sz = min(sz, j - i);
+    }
+    return sz == INT_MAX ? 0 : sz;
   }
 
-  int searchInsert(vector<int>& nums, int target) {
-    int s = 0, e = nums.size() - 1, m;
-    while(s <= e) {
-      m = s + (e - s) / 2;
-      if(nums[m] == target) {
-        return m;
-      } else if(nums[m] < target) {
-        s = m + 1;
+  int binarySearch(vector<int> & sums, int target, int l, int r) {
+    while(l <= r) {
+      int m = l + (r - l) / 2;
+      if(sums[m] > target) {
+        r = m - 1;
+      } else if(sums[m] < target) {
+        l = m + 1;
       } else {
-        e = m - 1;
+        return m;
       }
     }
-    return nums[m] > target ? m : m + 1;
+    return l;
   }
 };
